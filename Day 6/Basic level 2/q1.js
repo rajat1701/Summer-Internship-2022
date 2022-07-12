8128dc78a4941bdb9d068329ebfce21dadc1ed39
Rajat Malik <rajatmalik1701@gmail.com>
2022-07-12 12:43:39 +0530

    Code 1 for Basic-level 2 Added

---
+++ Day 6/Basic level 2/q1.js
@@ -0,0 +1,15 @@
+// Reverse an array? Input: [1, 2, 3, 4, 5, 6]
+const array = [1,2,3,4,5,6];
+console.log(`Array : ${array}`);
+console.log(`Reversed Array : ${array.reverse()}`);
+
+/*
+
+OUTPUT :
+
+[ /d/Projects/Summer-Internship-2022 ] (main)
+> node "d:\Projects\Summer-Internship-2022\Day 6\Basic level 2\q1.js"
+Array : 1,2,3,4,5,6
+Reversed Array : 6,5,4,3,2,1
+
+*/
