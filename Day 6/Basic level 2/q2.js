a5f33ab62125863119c9c720c2334387601e3a86
Rajat Malik <rajatmalik1701@gmail.com>
2022-07-12 12:50:22 +0530

    Code 2 for Basic-level 2 Added

---
+++ Day 6/Basic level 2/q2.js
@@ -0,0 +1,17 @@
+// Explain the properties of the join array method function via program
+const arr = [6, 3, 13];
+console.log(arr.join());
+console.log(arr.join(' - '));
+console.log(arr.join(' rajat '));
+
+/*
+
+OUTPUT :
+
+[ /d/Projects/Summer-Internship-2022 ] (main)
+> node "d:\Projects\Summer-Internship-2022\Day 6\Basic level 2\q2.js"
+6,3,13
+6 - 3 - 13
+6 rajat 3 rajat 13
+
+*/
